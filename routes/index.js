5e1b260c875dfe78dfe948c7e4c988e033a128fa
Matias98cs <matias98cs@gmail.com>
2022-08-29 17:21:16 -0300

    se creo la ruta de cities que usara el controller

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const citieRouter = require('./city')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json([]);
+  res.render('index', {title: 'My Tinerary'});
 });
 
-
-// htpp://localhost:3001/123
-//parametro de la URL
-router.get('/:id', function(req, res, next) {
-  res.json({
-    id: req.params.id
-  });
-});
+router.use('/cities', citieRouter)
 
 module.exports = router;
