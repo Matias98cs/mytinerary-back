eed1a7a51d80b160a0d551d31aa2103f508eff9e
Matias98cs <matias98cs@gmail.com>
2022-08-29 18:14:54 -0300

    se creo la propiedad de citycontroller con la read

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -15,6 +15,32 @@ const cityController = {
                 success: false
             })
         }
+    },
+    read: async(req, res) => {
+        const {id} = req.params
+        try{
+            let city = await City.findOne({_id:id})
+            //si city existe retorno un json con los datos
+            //si city no existe => city = {} retorno un json con 404
+            if(city){
+                res.status(200).json({
+                    message: 'Get one city',
+                    response: city,
+                    success: true
+                })
+            }else {
+                res.status(404).json({
+                    message: 'Couldt find city',
+                    success: false
+                })
+            }
+        }catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: "Could't obtain city",
+                success: false
+            })
+        }
     }
 }
 
