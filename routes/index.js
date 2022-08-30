04f17b774d62e7469b15acc3c215b0cab7a3fd35
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-08-30 13:53:38 -0300

    Modificamos ruta

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const citieRouter = require('./city')
+const citieRouter = require('./cityRouter')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {title: 'My Tinerary'});
 });
-
-router.use('/cities', citieRouter)
 
 module.exports = router;
