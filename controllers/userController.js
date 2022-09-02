cc087475dec108ff73be9c23a24dec71ff71254d
Matias98cs <matias98cs@gmail.com>
2022-09-02 18:04:01 -0300

    se muestra lo se creo el nuevo usuario

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,10 @@ const User = require('../models/User');
 const userController = {
     createUser: async(req, res) => {
         try{
-            await new User(req.body).save()
+            let user = await new User(req.body).save()
             res.status(201).json({
                 message: 'User created',
+                response: user,
                 success: true,
             })
         }catch{
