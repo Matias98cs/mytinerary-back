923db28611febc271875343eb0fad7dc1434f664
Matias98cs <matias98cs@gmail.com>
2022-09-02 18:47:24 -0300

    se modifico el controller de itine

--- controllers/itinerariController.js
+++ controllers/itinerariController.js
@@ -1,18 +1,45 @@
 const Itinerary = require('../models/Itinerary')
 
-
 const itineraryController = {
     createItinerary: async(req, res) => {
         try{
-            await new Itinerary(req.body).save()
+            let itinerary = await new Itinerary(req.body).save()
             res.status(201).json({
-                message: 'Crated',
+                message: 'Created',
+                response: itinerary,
                 success: true
             })
-        }catch{
+        }catch(error){
+            console.log(error)
             res.status(400).json({
-                message: 'Dont crated',
+                message: 'Dont created',
                 message: false
+            })
+        }
+    },
+    modifItinerary: async(req, res) => {
+        const {id} = req.params
+        const user = req.body
+        let userDb
+        try{
+            userDb = await new Itinerary.findOneAndUpdate({_id:id}, user, {new: true})
+            if(userDb){
+                res.status(200).json({
+                    message: 'User modified',
+                    response: userDb,
+                    success: true
+                })
+            }else{
+                res.status(404).json({
+                    message: "Couldn't modified user",
+                    success: false
+                })
+            }
+        }catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: "Could't obtain user",
+                success: false
             })
         }
     }
