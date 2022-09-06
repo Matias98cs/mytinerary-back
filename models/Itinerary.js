c389d0d29b58572303536526e01a89531056a7df
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-06 18:48:29 -0300

    Modificamos el Schema

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -3,11 +3,16 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
     name: { type: String, required: true },
     user: { type: String, },
-    city: { type: String, },
+    city: {
+        type: mongoose.Types.ObjectId,
+        ref: 'cities',
+        required: true
+    },
     price: { type: Number, required: true },
     likes: { type: Array, required: true },
     tags: { type: Array, required: true },
     duration: { type: Number, required: true },
+
 });
 
 const Itinerary = mongoose.model(
