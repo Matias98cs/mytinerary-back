b237c43bbcb3818788d37ce0233610724fa2728e
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-06 21:44:51 -0300

    Modificamos el Schema

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
     comment: { type: String, required: true },
     user: { type: String, },
-    itinerary: { type: String, },
+    itinerary: { 
+        type: mongoose.Types.ObjectId, 
+        ref: 'itineraries',
+        required: true
+    },
     
 });
 
