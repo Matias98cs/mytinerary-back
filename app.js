171777acf71e40cd60c17d9f9fcbac471a90021e
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-06 21:42:34 -0300

    Required

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var indexRouter = require('./routes/index');
 var cityRouter = require('./routes/cityRouter');
 var userRouter = require('./routes/userRouter')
 var itineraryRouter = require('./routes/itineraryRouter')
-
+var commentRouter = require('./routes/commentRouter')
 // var usersRouter = require('./routes/users');
 
 var app = express();
@@ -33,6 +33,7 @@ app.use('/', indexRouter)
 app.use('/cities', cityRouter)
 app.use('/auth', userRouter)
 app.use('/itineraries', itineraryRouter)
+app.use('/comments',commentRouter )
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
