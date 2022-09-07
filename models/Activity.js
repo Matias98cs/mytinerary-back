a7fa487f4fb1180a0565288708ffe91450bd37de
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-06 22:02:40 -0300

    Modificamos Schema

--- models/Activity.js
+++ models/Activity.js
@@ -3,8 +3,12 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
     name: { type: String, required: true },
     photo: { type: String, required: true },
-    itinerary: { type: String, },
-    
+    itinerary: {
+        type: mongoose.Types.ObjectId,
+        ref: 'itineraries',
+        required: true
+    },
+
 });
 
 const Activity = mongoose.model(
