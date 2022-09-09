883e29bd0c781b15311c5f75f8bbe42c7c5f6a13
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-09 19:02:45 -0300

    Se modifico nombre a authRouter.js

--- routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const {createUser} = require('../controllers/userController')
-
-router.post('/', createUser)
-
-module.exports = router;
