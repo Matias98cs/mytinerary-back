4ccabca6c5295d6297424691a2bf2c0cf36dfbce
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-12 13:22:35 -0300

    Agregamos la ruta verifyMail

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const {signUp} = require('../controllers/userController')
+const {signUp,verifyMail} = require('../controllers/userController')
 
 router.post('/signUp', signUp)
+router.get('/verify/:code', verifyMail)
 
 module.exports = router;
