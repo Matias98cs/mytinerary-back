6a37236d38b312a4eb8442998392916d95acbf31
Matias98cs <matias98cs@gmail.com>
2022-09-13 13:23:48 -0300

    se agrego validaciones al modelo de cities

--- models/City.js
+++ models/City.js
@@ -1,12 +1,43 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    city: { type: String, required: true },
-    country: { type: String, required: true },
-    photo: { type: String, required: true },
-    population: { type: Number, required: true },
-    fundation: { type: Date, required: true },
-    description: { type: String },
+    city: { 
+        type: String,
+        required: true,
+        min: 4,
+        max: 50,
+     },
+    country: { 
+        type: String, 
+        required: true,
+        min: 4,
+        max: 50,
+     },
+    photo: { 
+        type: String,
+        required: true,
+        validate: function(value){
+            if(!value.startsWith('http')){
+                throw new Error('La URL debe comenzar con http')
+            }
+        }
+     },
+    population: { 
+        type: Number,
+        required: true,
+        min: 1000,
+        max: 1000000000
+     },
+    fundation: { 
+        type: Date,
+        required: true,
+        max: new Date()
+     },
+    description: { 
+        type: String,
+        min: 10,
+        max: 300
+     },
 });
 
 const City = mongoose.model(
