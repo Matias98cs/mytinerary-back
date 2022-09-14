7fe136c9949ba4f0ad030ac9985dfbb06c3f8de5
Matias98cs <matias98cs@gmail.com>
2022-09-14 00:09:19 -0300

    se agrego una ruta para signout

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const {signUp, verifyMail, singIn} = require('../controllers/userController')
+const {signUp, verifyMail, singIn, singOut} = require('../controllers/userController')
 
 router.post('/signup', signUp)
 router.post('/signin', singIn)
+router.post('/signout', singOut)
 router.get('/verify/:code', verifyMail)
 
 module.exports = router;
