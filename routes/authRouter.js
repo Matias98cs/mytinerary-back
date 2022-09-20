088dcfa77cbc49f03942615534b3e1deddde5793
Matias98cs <matias98cs@gmail.com>
2022-09-20 16:04:45 -0300

    se agrego una ruta para la verificacion del token

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,12 +2,12 @@ var express = require('express');
 const passport = require('../config/passport');
 var router = express.Router();
 
-const {signUp, verifyMail, singIn, singOut} = require('../controllers/userController')
+const {signUp, verifyMail, singIn, singOut, veryToken} = require('../controllers/userController')
 
 router.post('/signup', signUp)
 router.post('/signin', singIn)
 router.post('/signout', singOut)
 router.get('/verify/:code', verifyMail)
-// router.get('/token', passport.authenticate('jwt', {session: false}), verifyToken)
+router.get('/token', passport.authenticate('jwt', {session: false}), veryToken)
 
 module.exports = router;
