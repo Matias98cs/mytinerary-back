18a77d9e5f1f9ab05a32897e0a14813382debcab
Matias98cs <matias98cs@gmail.com>
2022-09-20 15:40:31 -0300

    algo

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const passport = require('../config/passport');
 var router = express.Router();
 
 const {signUp, verifyMail, singIn, singOut} = require('../controllers/userController')
@@ -7,5 +8,6 @@ router.post('/signup', signUp)
 router.post('/signin', singIn)
 router.post('/signout', singOut)
 router.get('/verify/:code', verifyMail)
+// router.get('/token', passport.authenticate('jwt', {session: false}), verifyToken)
 
 module.exports = router;
