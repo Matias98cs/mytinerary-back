4b03e5e9697c915311f6a2cf09b91507ba164303
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-21 18:26:10 -0300

    Validamos schema comment

--- models/Comment.js
+++ models/Comment.js
@@ -1,14 +1,23 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    comment: { type: String, required: true },
-    user: { type: String, },
-    itinerary: { 
-        type: mongoose.Types.ObjectId, 
+    comment: { 
+        type: String,
+        required: true,
+        min: 1,
+        max: 100
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'users',
+        requiered: true
+    },
+    itinerary: {
+        type: mongoose.Types.ObjectId,
         ref: 'itineraries',
         required: true
     },
-    
+
 });
 
 const Comment = mongoose.model(
