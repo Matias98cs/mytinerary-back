252e1abb4d3e2f41ae5ae997d9b058bd83112210
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-21 17:27:12 -0300

    Se creo validador con joi

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,9 +1,17 @@
 const Activity = require('../models/Activity')
 const City = require('../models/City')
+const Joi = require("joi");
+
+const validator = Joi.object({
+    name: Joi.string().min(5).max(50).message("INVALID_NAME"),
+    photo: Joi.string().uri().message("INVALID URL"),
+    itinerary: Joi.string(),
+});
 
 const activityController = {
     createActivity: async (req, res) => {
         try {
+            let result = await validator.validateAsync(req.body)
             let activity = await new Activity(req.body).save()
             res.status(201).json({
                 message: 'Created',
@@ -13,8 +21,8 @@ const activityController = {
         } catch (error) {
             console.log(error)
             res.status(400).json({
-                message: 'Dont created',
-                message: false
+                message: error.message,
+                success: false
             })
         }
     },
@@ -50,11 +58,12 @@ const activityController = {
     updateActivity: async(req, res) => {
         const {id} = req.params
         try{
+            let result = await validator.validateAsync(req.body)
             let activity = await Activity.findOne({_id: id})
             if (activity){
                 await Activity.findOneAndUpdate({_id:id}, req.body, {new: true})
                 res.status(200).json({
-                    message: 'Activiy update',
+                    message: 'Activity update',
                     success: true,
                     response: activity
                 })
@@ -67,7 +76,7 @@ const activityController = {
         }catch(error){
             console.log(error)
             res.status(400).json({
-                message: "Couldn't obtain update",
+                message: error.message,
                 success: false
             })
         }
