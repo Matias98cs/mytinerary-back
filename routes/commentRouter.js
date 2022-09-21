e56866366bf4546c7469155dc8bce86778281dd8
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-21 18:27:17 -0300

    Agregamos passport a la ruta

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const passport = require ('../config/passport')
 
 const {createComment, readAll} = require('../controllers/commentController')
 
 router.get('/query', readAll)
-router.post('/', createComment)
+router.post('/', passport.authenticate('jwt',{session:false}), createComment)
 
 
 module.exports = router;
