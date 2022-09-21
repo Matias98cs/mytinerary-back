c90997f8aa0a76923e8bb551490441e8ad12d424
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-21 17:28:02 -0300

    Se agrego validador al esquema

--- models/Activity.js
+++ models/Activity.js
@@ -1,8 +1,21 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    name: { type: String, required: true },
-    photo: { type: String, required: true },
+    name: { 
+        type: String,
+        required: true,
+        min: 5,
+        max: 50
+    },
+    photo: {
+        type: String,
+        required: true,
+        validate: function (value) {
+            if (!value.startWith('http')) {
+                throw new Error('La URL debe comenzar con http')
+            }
+        }
+    },
     itinerary: {
         type: mongoose.Types.ObjectId,
         ref: 'itineraries',
