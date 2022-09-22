1a106ff11e6d0d64c9686b53f25503fb7ce7b00a
Matias98cs <matias98cs@gmail.com>
2022-09-22 12:56:41 -0300

    se creo una ruta para borrar comentarios con el passport de auth

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 const passport = require ('../config/passport')
 
-const {createComment, readAll, modifyComment} = require('../controllers/commentController');
+const {createComment, readAll, modifyComment, deleteCommnet} = require('../controllers/commentController');
 const { route } = require('./itineraryRouter');
 
 router.get('/query', readAll)
 router.post('/', passport.authenticate('jwt',{session:false}), createComment)
 router.patch('/:id', passport.authenticate('jwt', {session:false}), modifyComment) 
+router.delete('/:id', passport.authenticate('jwt', {session:false}), deleteCommnet )
 
 module.exports = router;
