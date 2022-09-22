8b8f856c7b7c2db5b5deca7ca372aa81d198cc50
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-22 12:52:58 -0300

    Se autentico las rutas

--- routes/cityRouter.js
+++ routes/cityRouter.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const passport = require ('../config/passport')
 
 const {create, readId, modify, deletecity, readAll, citiesAll} = require('../controllers/cityController')
 
 router.get('/', readAll)
 router.get('/', citiesAll)
-router.post('/', create)
+router.post('/',passport.authenticate('jwt',{session:false}), create)
 router.get('/:id', readId)
-router.patch('/:id', modify)
-router.delete('/:id', deletecity)
+router.patch('/:id',passport.authenticate('jwt',{session:false}), modify)
+router.delete('/:id',passport.authenticate('jwt',{session:false}), deletecity)
 
 module.exports = router;
