225f60837dde00a6127a75bb679ebc251aefe514
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-21 21:35:18 -0300

    Se agregaron rutas a comment

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 const passport = require ('../config/passport')
 
-const {createComment, readAll} = require('../controllers/commentController')
+const {createComment, readAll, modifyComment} = require('../controllers/commentController');
+const { route } = require('./itineraryRouter');
 
 router.get('/query', readAll)
 router.post('/', passport.authenticate('jwt',{session:false}), createComment)
-
+router.patch('/:id', passport.authenticate('jwt', {session:false}), modifyComment) 
 
 module.exports = router;
